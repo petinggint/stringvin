22e28b0776049b14350410d2b5ad870384aa45ce
petinggint <155607304+petinggint@users.noreply.github.com>
2025-06-07 14:22:41 +0700

    s config.py

--- config.py
+++ config.py
@@ -11,5 +11,5 @@ API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN")
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
-OWNER_ID = int(getenv("OWNER_ID", 5510539512))
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/infomusiccalvin")
+OWNER_ID = int(getenv("OWNER_ID", 6364520406))
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+-eco0zKCmlpmZGJl")
